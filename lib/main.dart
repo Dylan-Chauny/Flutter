c1f9d265e82d0f290321b8641e333404788b3d9f
Dylan-Chauny <ChaunyDylan96@gmail.com>
2019-12-02 13:35:38 +0100

    Remove the debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Fluteo App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
